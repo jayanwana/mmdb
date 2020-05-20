2df08fd94c54580bcf0b47d6d2623c2d928a1df6
Aj <jayanwana@gmail.com>
2020-05-20 13:27:13 +0100

    Include Marketmodule and MongooseModel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MarketModule } from './market/market.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MarketModule, MongooseModule.forRoot(
+    'mongodb+srv://jayanwana:Jp5cGb8nW2mTT3Ow@cluster0-fgqw7.gcp.mongodb.net/market?retryWrites=true&w=majority'
+  )],
   controllers: [AppController],
   providers: [AppService],
 })
