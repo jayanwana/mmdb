b609f968d2cf01ffa896e7b1da8b24b33cb4fefd
Aj <jayanwana@gmail.com>
2020-05-21 19:45:05 +0100

    Authentication and admin login functionality

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Request, Post, Body, UseGuards, Logger} from '@nestjs/common';
+import { UsersService } from './users/users.service';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+  private readonly logger = new Logger(AppController.name)
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  };
+
+  @Post('register')
+  async register(@Body('email') email: string, @Body('password') password:string) {
+    const user = await this.usersService.createUser(email, password);
+    if (user) {
+      return user.id as string
+    }
+  };
+
+  @UseGuards(JwtAuthGuard)
+  @Post('test')
+  async test(@Body('email') email: string) {
+    const user = await this.usersService.findUser(email)
+    if (!user) {return {}}
+    return user
   }
-}
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MarketModule } from './market/market.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MarketModule, MongooseModule.forRoot(
-    'mongodb+srv://jayanwana:Jp5cGb8nW2mTT3Ow@cluster0-fgqw7.gcp.mongodb.net/market?retryWrites=true&w=majority'
-  )],
+    UsersModule, MarketModule, AuthModule, MongooseModule.forRoot(
+    'mongodb+srv://jayanwana:Jp5cGb8nW2mTT3Ow@cluster0-fgqw7.gcp.mongodb.net/market?retryWrites=true&w=majority'),
+    MongooseModule.forRoot(
+    'mongodb+srv://jayanwana:Jp5cGb8nW2mTT3Ow@cluster0-fgqw7.gcp.mongodb.net/user?retryWrites=true&w=majority'),
+    MulterModule.register({dest: 'static/MarketImages'})
+],
   controllers: [AppController],
   providers: [AppService],
 })
